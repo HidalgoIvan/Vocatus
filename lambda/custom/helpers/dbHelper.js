d6d68e5e3882ab30ee24c428b394381a6ec2f2fc
HidalgoIvan <A01651425@itesm.mx>
2019-04-14 22:50:25 -0500

    ultimo avance

--- lambda/custom/helpers/dbHelper.js
+++ lambda/custom/helpers/dbHelper.js
@@ -1,16 +1,39 @@
 var AWS = require("aws-sdk");
 AWS.config.update({region: "us-east-1"});
-const tableName = "vocatus-names";
-
+const tableName = "vocatus-playerNames";
+const questionTableName = "vocatus-questions";
 var dbHelper = function () { };
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-dbHelper.prototype.addName = (name, userID) => {
+dbHelper.prototype.getQuestions = (question_Id) => {
+    return new Promise((resolve, reject) => {
+        const params = {
+            TableName: questionTableName,
+            KeyConditionExpression: "#questionId = :question_Id",
+            ExpressionAttributeNames: {
+                "#questionId": "questionId"
+            },
+            ExpressionAttributeValues: {
+                ":question_Id": question_Id
+            }
+        }
+        docClient.query(params, (err, data) => {
+            if (err) {
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                return reject(JSON.stringify(err, null, 2))
+            } 
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+            resolve(data.Items)
+        })
+    });
+}
+
+dbHelper.prototype.addName = (pName, userID) => {
     return new Promise((resolve, reject) => {
         const params = {
             TableName: tableName,
             Item: {
-              'name' : name,
+              'playerName' : pName,
               'userId': userID
             }
         };
@@ -25,16 +48,16 @@ dbHelper.prototype.addName = (name, userID) => {
     });
 }
 
-dbHelper.prototype.getNames = (userID) => {
+dbHelper.prototype.getNames = (userId) => {
     return new Promise((resolve, reject) => {
         const params = {
             TableName: tableName,
-            KeyConditionExpression: "#userID = :user_id",
+            KeyConditionExpression: "#userId = :user_id",
             ExpressionAttributeNames: {
-                "#userID": "userId"
+                "#userId": "userId"
             },
             ExpressionAttributeValues: {
-                ":user_id": userID
+                ":user_id": userId
             }
         }
         docClient.query(params, (err, data) => {
@@ -49,15 +72,15 @@ dbHelper.prototype.getNames = (userID) => {
     });
 }
 
-/*dbHelper.prototype.removeMovie = (movie, userID) => {
+dbHelper.prototype.removeName = (pName, userID) => {
     return new Promise((resolve, reject) => {
         const params = {
             TableName: tableName,
             Key: {
                 "userId": userID,
-                "movieTitle": movie
+                "playerName": pName
             },
-            ConditionExpression: "attribute_exists(movieTitle)"
+            ConditionExpression: "attribute_exists(playerName)"
         }
         docClient.delete(params, function (err, data) {
             if (err) {
@@ -69,6 +92,6 @@ dbHelper.prototype.getNames = (userID) => {
             resolve()
         })
     });
-}*/
+}
 
 module.exports = new dbHelper();
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -5,25 +5,81 @@ const Alexa = require('ask-sdk');
 const dbHelper = require('./helpers/dbHelper');
 const GENERAL_REPROMPT = "What would you like to do?";
 const dynamoDBTableName = "vocatus-names";
+async function getRandomQuestion(handlerInput)
+{
+  var speechText = ""
+  const { requestEnvelope, attributesManager } = handlerInput;
+  const sessionAttributes = attributesManager.getSessionAttributes();
+  var questionId = Math.floor(Math.random() * 10) + 1; 
+  return dbHelper.getQuestions(questionId)
+      .then((data) => {
+        var question = data.map(e => e.text)
+        speechText += data[0].text;
+        speechText += " " + data[0].options;
+        Object.assign(sessionAttributes, {
+        correctAnswer: data[0].correctAnswer
+      });
+        return speechText;
+    }
+  )
+}
+async function getRandomName(handlerInput, userID)
+{
+  var speechText = ""
+  const { requestEnvelope, attributesManager } = handlerInput;
+  const sessionAttributes = attributesManager.getSessionAttributes();
+   return dbHelper.getNames(userID)
+      .then((data) => {
+        if (data.length == 0) {
+          speechText = "Aún no has guardado ningún nombre"
+        } else {
+          var nameArray = data.map(e => e.playerName).join(",").split(",");
+          speechText = nameArray[Math.floor(Math.random()*nameArray.length)];
+        }
+        return speechText
+      })
+}
+async function getAllNames(handlerInput, userID)
+{
+  var names = "";
+  return dbHelper.getNames(userID)
+      .then((data) => {
+          names += data.map(e => e.playerName).join(", ")
+        return names
+      })
+}
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const responseBuilder = handlerInput.responseBuilder;
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Hola, soy Vocatus tu skill específica para las pedar y poner el ambiente, dime los nombres de los jugadores y cuando estés listo para jugar di, inicia el juego';
+    const repromptText = 'Dime un nombre';
 
-        let say = 'Bienvenido a vocatus, tu skill para poner el ambiente en las pedas.';
-
-
-        return responseBuilder
-            .speak(say)
-            .reprompt('try again, ' + say)
-            .withStandardCard('Welcome!')
-            .getResponse();
-    },
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(repromptText)
+      .getResponse();
+  },
 };
-
+const StartTriviaIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'StartTrivia';
+  },
+  async handle(handlerInput) {
+    const {responseBuilder } = handlerInput;
+    const userID = handlerInput.requestEnvelope.context.System.user.userId;
+    var names = await getAllNames(handlerInput, userID);
+    var speechText = "Prepárense " + names + " para un juego en el que se les harán varias\
+    preguntas y ustedes deberán contestar tan bien como puedan para evitar salir en coma etílico";
+    var response = responseBuilder
+      .speak(speechText)
+      .getResponse();
+    response.shouldEndSession = false;
+    return response;
+  }
+};
 const SaveNameIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -34,6 +90,9 @@ const SaveNameIntentHandler = {
     const userID = handlerInput.requestEnvelope.context.System.user.userId; 
     const slots = handlerInput.requestEnvelope.request.intent.slots;
     const name = slots.Name.value;
+    console.log("NAMES:" + slots.Name);
+    console.log("NAME VALUES: " + slots.Name.value);
+    console.log("SLOTS:" + slots);
     return dbHelper.addName(name, userID)
       .then((data) => {
         const speechText = `Acabas de añadir a ${name}.`;
@@ -66,7 +125,7 @@ const TellNameIntentHandler = {
         if (data.length == 0) {
           speechText = "Aún no has guardado ningún nombre"
         } else {
-          speechText += data.map(e => e.name).join(", ")
+          speechText += data.map(e => e.playerName).join(", ")
         }
         return responseBuilder
           .speak(speechText)
@@ -81,34 +140,93 @@ const TellNameIntentHandler = {
       })
   }
 }
+const HandleGuessIntentHandler = {
 
-/*const RemoveMovieIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'RemoveMovieIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'HandleGuess';
+  },
+  async handle(handlerInput) {
+    var correctAnswer = "";
+    const { responseBuilder } = handlerInput;
+    var speechText = "";
+    try{
+    const { requestEnvelope, attributesManager } = handlerInput;
+    
+    const request = handlerInput.requestEnvelope.request;
+    let guess = request.intent.slots.Answer.value;
+    const sessionAttributes = attributesManager.getSessionAttributes();
+    correctAnswer = sessionAttributes['correctAnswer'];
+      if(guess.includes(correctAnswer.toLowerCase()))//Respuesta correcta
+      {
+
+        speechText = `<speak>¿${guess}?, La respuesta es...<break time="1s"/>¡Correcta!<audio src="soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_positive_response_03"/></speak>`;
+      }else{
+        var seconds = Math.floor(Math.random()*15) + 2;
+        var alcohols = ["cerveza","vodka","tequila","una cubita", "nesquick por la nariz", "tonayan", "besar al de al lado", "darle de tomar al de al lado", "fourloko", "kosaco", "aguas locas", "la bebida del de al lado", "el vaso más lleno", "alcohol con mayor grado","lamerle el pie al más chaparro", "quitarte la playera","gemir","yakult"];
+        var drink = alcohols[Math.floor(Math.random()*alcohols.length)];
+        speechText = `<speak>¿${guess}?, La respuesta es...<break time="1s"/>¡Incorrecta!<audio src='soundbank://soundlibrary/human/amzn_sfx_crowd_boo_03'/>${seconds} segundos de ${drink}</speak>`;
+      }
+    }
+    catch(err)
+    {
+      console.log("ERROR AYUDA RESPUESTAS AHH : " + err);
+    }
+        var response = responseBuilder
+          .speak(speechText)
+          .getResponse();
+        response.shouldEndSession = false;
+        return response;
+    }
+}
+const GetQuestionIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'GetQuestion';
+  },
+  async handle(handlerInput) {
+    const {responseBuilder } = handlerInput;
+    const userID = handlerInput.requestEnvelope.context.System.user.userId; 
+    const { requestEnvelope, attributesManager } = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
+    var questionText = await getRandomQuestion(handlerInput);
+    var playerName = await getRandomName(handlerInput, userID);
+    var speechText = playerName + ", " + questionText;
+    var response = responseBuilder
+      .speak(speechText)
+      .getResponse();
+    response.shouldEndSession = false;
+    return response;
+  }
+}
+const RemoveNameIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && handlerInput.requestEnvelope.request.intent.name === 'RemoveName';
   }, 
   handle(handlerInput) {
     const {responseBuilder } = handlerInput;
     const userID = handlerInput.requestEnvelope.context.System.user.userId; 
     const slots = handlerInput.requestEnvelope.request.intent.slots;
-    const movieName = slots.MovieName.value;
-    return dbHelper.removeMovie(movieName, userID)
+    const name = slots.Name.value;
+    return dbHelper.removeName(name, userID)
       .then((data) => {
-        const speechText = `You have removed movie with name ${movieName}, you can add another one by saying add`
+        const speechText = `Has borrado a ${name} del juego, qué mala onda`
         return responseBuilder
           .speak(speechText)
           .reprompt(GENERAL_REPROMPT)
           .getResponse();
       })
       .catch((err) => {
-        const speechText = `You do not have movie with name ${movieName}, you can add it by saying add`
+        console.log(err);
+        const speechText = `No hay ningún jugador llamado ${name} actualmente`
         return responseBuilder
           .speak(speechText)
           .reprompt(GENERAL_REPROMPT)
           .getResponse();
       })
   }
-}*/
+}
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
@@ -172,11 +290,17 @@ exports.handler = skillBuilder
     LaunchRequestHandler,
     SaveNameIntentHandler,
     TellNameIntentHandler,
+    GetQuestionIntentHandler,
+    StartTriviaIntentHandler,
+    HandleGuessIntentHandler,
+    RemoveNameIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
   )
   .addErrorHandlers(ErrorHandler)
   .withTableName(dynamoDBTableName)
+  .withAutoCreateTable(true)
+  .withTableName('vocatus-questions')
   .withAutoCreateTable(true)
   .lambda();
